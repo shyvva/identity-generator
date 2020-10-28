ad0d1681c1d9ba481dad6e622b001550db2018d1
Dawid Przydryga <shivva90@gmail.com>
2020-10-28 09:57:42 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const os = require('os');
+const fs = require('fs');
+
+const genders = ['m', 'f'];
+
+const maleNames = [
+    'John',
+    'Matthew',
+    'Carl',
+    'Paul',
+    'Steve',
+    'David',
+    'James',
+    'Janusz',
+    'Freddy',
+    'Bruce',
+    'Tom',
+    'Luise'
+]
+
+const famaleNames = [
+    'Victoria',
+    'Johanna',
+    'Megan',
+    'Scarllet',
+    'Janet',
+    'Anna',
+    'Hannah',
+    'Elizabeth',
+    'Uma',
+    'Katarina',
+    'Evelyn',
+    'Fiora'
+]
+
+const lastNames = [
+    'Kowalsky',
+    'Snow',
+    'Black',
+    'Gray',
+    'Skywalker',
+    'Potter',
+    'Mercury',
+    'Hamilton',
+    'Jackson',
+    'Johanson',
+    'Ragnarson',
+    'Novak'
+]
